86e244de4f2376be65c980f2bce13dcfeaf26a1a
Kiattipan Muangyai <playicest@gmail.com>
2023-01-17 23:03:38 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+require('express-async-errors')
+
+//express
+const express = require('express');
+const app = express();
+
+//database
+const connectDB = require('./db/connect');
+const port = process.env.PORT | 3000
+
+//middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+//packages
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
+app.use(express.json());
+
+
+// routes
+app.get('/', (req, res) => {
+    res.send('HELLO! EXPRESssS')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const startServer = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is listening on port ${port}...`))
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+startServer();
