0b6c4bcc684e16355d9d42ff767c5d908ea8fb7a
Kiattipan Muangyai <playicest@gmail.com>
2023-01-18 21:56:04 +0700

    login routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,7 +27,27 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('Login User');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid Credentials');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid Crendentials');
+  }
+  // eslint-disable-next-line no-underscore-dangle
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
