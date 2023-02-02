ac99a7b8358aef71c84d9915710790077a7e5d67
Kiattipan Muangyai <playicest@gmail.com>
2023-02-02 23:37:19 +0700

    Add Order Controllers Function and Routes

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 const productRouter = require('./routes/productRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
 
 const port = process.env.PORT || 5000;
 
@@ -47,6 +48,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
+
 const connectDB = (url) => mongoose.connect(url);
 
 module.exports = connectDB;
